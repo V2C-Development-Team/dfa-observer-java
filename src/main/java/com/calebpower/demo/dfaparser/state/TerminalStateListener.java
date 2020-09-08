13af81bd7f98a578d73a63a01071e690fe6f657d
Caleb L. Power <cpower@axonibyte.com>
2020-09-08 07:39:18 -0500

    added bootloader and machine skeleton; added terminal state listener interface, but didn't implement it

---
+++ src/main/java/com/calebpower/demo/dfaparser/state/TerminalStateListener.java
@@ -0,0 +1,32 @@
+/*
+ * Copyright (c) 2020 Caleb L. Power. All rights reserved.
+ *
+ *   Licensed under the Apache License, Version 2.0 (the "License");
+ *   you may not use this file except in compliance with the License.
+ *   You may obtain a copy of the License at
+ *
+ *   https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.calebpower.demo.dfaparser.state;
+
+/**
+ * Listens for the machine to reach a terminal state.
+ * 
+ * @author Caleb L. Power
+ */
+public interface TerminalStateListener {
+  
+  /**
+   * Performs some action when a terminal state is reached.
+   * 
+   * @param state the terminal state
+   */
+  public void onTerminalState(State state);
+  
+}
